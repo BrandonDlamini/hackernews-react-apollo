5b00866c6522edba99264f7c8d532ff557f5aa36
Brandon <brandon.dlamini95@gmail.com>
2020-12-03 13:40:47 +0200

    added search functions

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,6 +3,28 @@ import {withApollo} from 'react-apollo'
 import gql from 'graphql-tag'
 import Link from './Link'
 
+const FEED_SEARCH_QUERY =gql `
+query FeedSearchQuery($filter:String!){
+    feed(filter: $filter) {
+        links {
+            id
+            url
+            description
+            createdAt
+            postedBy {
+                id
+                name
+            }
+            votes {
+                id
+                user {
+                    id
+                }
+            }
+        }
+    }
+}`
+
 class Search extends Component {
 
     state ={
@@ -17,16 +39,25 @@ class Search extends Component {
                     Search<input
                     type = "text"
                     onChange = {e=> this.setState({filter: e.target.value}) }/>
-                    <button onClick ={()=> this.ExecuteSearch()}>Ok</button>
+                    <button onClick ={()=> this._executeSearch()}>Ok</button>
                 </div>
                 {this.state.links.map((link, index) => (<Link key={link.id} link={link} index ={index}/> ))}
             </div>
         )
     }
 
-    _excecuteSearch = async () => {
+    _executeSearch = async () => {
 
-    }
+        const { filter } = this.state
+        
+        const result = await this.props.client.query({
+          query: FEED_SEARCH_QUERY,
+          variables: { filter },
+        })
+        
+        const links = result.data.feed.links
+        this.setState({ links })
+      }
 }
 
 export default withApollo(Search)
